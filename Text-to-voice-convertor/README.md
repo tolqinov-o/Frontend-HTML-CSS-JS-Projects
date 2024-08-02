1f0f780eb5fe9574fd038fd7d2c2293cecff0ac2
T.Oqilbek <tolqinovoqilbek90@gmail.com>
2024-08-02 11:12:06 +0500

    My_Frontend || Project-23

---
+++ Text-to-voice-convertor/README.md
@@ -0,0 +1,35 @@
+# Frontend Text-To-Voice-Convertor Project
+My_Frontend || Project-23
+
+## Screenshots
+<table>
+    <tr>
+        <td>
+            <img src="./img/img1.jpg" alt="Frontend-Projects">
+        </td>
+        <td>
+            <img src="./img/img2.jpg" alt="Frontend-Projects">
+        </td>
+    </tr>
+</table>
+
+## Demo
+
+[Live Demo](https://frontend-project-23.netlify.app)
+
+## Tech Stack
+
+- HTML
+- CSS
+- JavaScript
+- Responsive
+
+## Authors
+
+- [T.Oqilbek](https://www.github.com/tolqinov-o)
+
+### My Text-To-Voice-Convertor Demo Link
+
+```
+https://frontend-project-23.netlify.app
+```
---
+++ Text-to-voice-convertor/js/index.js
@@ -0,0 +1,19 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[3];
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option (voice.name, i)));
+}
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
+document.querySelector("button").addEventListener("click", function() {
+    speech.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
